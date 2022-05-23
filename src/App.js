35198f1163169c0d337081680f5d038617a6906a
leozinhoK <leonardokruel046@gmail.com>
2022-05-23 20:32:09 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          O MELHOR DO MUNDO / <code>VIDA DE GREMISTA /  </code> COLORADO PASSA MAL.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://gremio.net/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          É O GREMIO PAI 
         </a>
       </header>
     </div>
